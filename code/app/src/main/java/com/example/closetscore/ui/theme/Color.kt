8e4e2e803d9b68114fb7016089ec3931a1608d33
unknown <oksumkadim@gmail.com>
2026-01-13 12:56:07 +0100

    Updated Theme

--- code/app/src/main/java/com/example/closetscore/ui/theme/Color.kt
+++ code/app/src/main/java/com/example/closetscore/ui/theme/Color.kt
@@ -6,18 +6,8 @@ val Green = Color(0xFF214E34)
 val White = Color(0xFFFFFFFF)
 val Black = Color(0xFF000000)
 val Grey = Color(0xFFF2F2F2)
-val DarkGrey = Color(0xFF6D6D6D)
-
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
+val DarkGrey = Color(0xFF828282)
+val DarkestGrey = Color(0xFF424242)
 val Red = Color(0xFFBF3558)
-
 val Orange = Color(0xFFF96900)
-
 val LightGreen = Color(0xFF5FAD56)
