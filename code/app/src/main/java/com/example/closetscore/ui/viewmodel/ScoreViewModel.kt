16bd391ae019c0adfc932029aab0ee81e0aee28a
unknown <oksumkadim@gmail.com>
2026-01-20 12:13:55 +0100

    Seperate caluclation from viewmodel

--- code/app/src/main/java/com/example/closetscore/ui/viewmodel/ScoreViewModel.kt
+++ code/app/src/main/java/com/example/closetscore/ui/viewmodel/ScoreViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.AP
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.closetscore.coroutines.calculateLogic
 import com.example.closetscore.data.Item
 import com.example.closetscore.data.ItemRepository
 import com.example.closetscore.db.BrandType
@@ -31,52 +32,4 @@ class ScoreViewModel(repository: ItemRepository) : ViewModel() {
             initialValue = 0
         )
 
-    private fun calculateLogic(items: List<Item>): Int {
-        var score = 0
-        if(items.isEmpty()){
-            return 0
-        }
-        items.forEach { item ->
-            score += calculateValue(item)
-        }
-        val average = score / items.size
-        return average.coerceIn(0, 100)
-    }
-    fun calculateValue(item: Item): Int {
-        var score = 50
-        if (item.isSecondHand) {
-            score += 20
-        }
-
-        when (item.brandType) {
-            BrandType.FAST_FASHION -> score -= 20
-            BrandType.ECO_SUSTAINABLE -> score += 15
-            BrandType.STANDARD -> score += 0
-        }
-
-        when (item.material) {
-            MaterialType.NATURAL -> score += 10
-            MaterialType.SYNTHETIC -> score -= 10
-            MaterialType.MIXED -> score -= 5
-        }
-
-        score += item.wearCount/3
-
-        when (item.status) {
-            ItemStatus.TRASHED -> {
-                if (item.wearCount > 50) {
-                    score += 0
-                } else if (item.wearCount > 30) {
-                    score -= 10
-                } else {
-                    score -= 50
-                }
-            }
-            ItemStatus.SOLD, ItemStatus.DONATED -> score += 10
-            ItemStatus.ACTIVE -> score+=0
-            ItemStatus.LOST -> score-=1
-        }
-        return score
-    }
 }
-
